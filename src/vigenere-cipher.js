aedd6893143b157ef00d805668496ab2687c91ae
dvobabsi <dvobabsi@gmail.com>
2025-01-06 19:37:53 +0200

    feat: add solution vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,11 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    const upperMessage = message.toUpperCase();
+    const upperKey = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < upperMessage.length; i++) {
+      const char = upperMessage[i];
+
+      if (/[A-Z]/.test(char)) {
+        const messageCharCode = char.charCodeAt(0) - 65;
+        const keyCharCode = upperKey[keyIndex % upperKey.length].charCodeAt(0) - 65;
+        const encryptedCharCode = (messageCharCode + keyCharCode) % 26;
+        result += String.fromCharCode(encryptedCharCode + 65);
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+
+    const upperMessage = encryptedMessage.toUpperCase();
+    const upperKey = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < upperMessage.length; i++) {
+      const char = upperMessage[i];
+
+      if (/[A-Z]/.test(char)) {
+        const encryptedCharCode = char.charCodeAt(0) - 65;
+        const keyCharCode = upperKey[keyIndex % upperKey.length].charCodeAt(0) - 65;
+        const decryptedCharCode = (encryptedCharCode - keyCharCode + 26) % 26;
+        result += String.fromCharCode(decryptedCharCode + 65);
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 }
 
